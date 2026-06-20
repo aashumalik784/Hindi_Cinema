e95de4ba57b371ef4db63011f78b6347c972d410
aashumalik784 <YOUR_EMAIL@gmail.com>
2026-06-20 20:39:25 +0530

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'archive.org',
+      },
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
